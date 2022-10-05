c52d7283c5927f1029b729452aa3315990aa1f63
Derek Kropff <u19015501@tuks.co.za>
2022-10-05 11:08:35 +0200

    getFlag has been implemented

--- source/ByteBooleanArrayFlag.java
+++ source/ByteBooleanArrayFlag.java
@@ -1,28 +1,66 @@
 //bArray[secondaryIndex] = index(0-7) for secondary index;
 
 public class ByteBooleanArrayFlag {
-    private byte[] bArray;
+    
+    private byte boolArray = (byte)129;
+
     public ByteBooleanArrayFlag() {}
     public ByteBooleanArrayFlag(boolean[] bs) {}
 
-    public int[] AddFlag(boolean b) {// return index in form of (0-7, 0-Number of bytes)
-
+    public boolean getFlag(int index) {//returns the flag value at the specified index
+        //make sure inupt is correct
+        if(index < 0 || index > 7) {
+            return false;
+        }
+        
+        byte tB = boolArray;
+        
+        //read indexed boolean value from a byte
+        for(int i = 7; i >= 0; i--) {
+            if(Math.pow(2, i) <= (tB & 0xff)) {
+                if(i == index) {
+                    return true;
+                } else {
+                    tB = (byte)((int)(tB & 0xff) - Math.pow(2, i));
+                }
+            } else {
+                if(i == index) {
+                    return false;
+                }
+            }
+        }
+        return false;// just to make sure we dont die
     }
 
-    public boolean getFlag(int index, int secondaryIndex) {//returns the flag value at the specified index
-
-    }
-
-    public boolean setFlag(int index, int secondaryIndex, boolean value) {//returns true if flag was set correctly
-
-    }
-
-    public boolean removeFlag(int index, int secondaryIndex) {
-
+    public boolean setFlag(int index, boolean value) {//returns true if flag was set correctly
+        //make sure inupt is correct
+        if(index < 0 || index > 7) {
+            return false;
+        }
+        
+        byte tB = boolArray;
+        
+        //read indexed boolean value from a byte
+        for(int i = 7; i >= 0; i--) {
+            if(Math.pow(2, i) <= (tB & 0xff)) {
+                if(i == index) {
+                    System.out.println(true);//
+                    return true;
+                } else {
+                    tB = (byte)((int)(tB & 0xff) - Math.pow(2, i));
+                }
+            } else {
+                if(i == index) {
+                    System.out.println(false);//
+                    return false;
+                }
+            }
+        }
+        return false;// just to make sure we dont die
     }
 
     protected void finalize()  
     {
         System.out.println("Object is destroyed by the Garbage Collector");  
     }
-}
+} 
--- source/Main.java
+++ source/Main.java
@@ -1,4 +1,29 @@
 
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        byte b = (byte)129;
+        byte tB = b;
+        //retrieve index 6
+        int x = 6;
+
+        //read indexed boolean value from a byte
+        for(int i = 7; i >= 0; i--) {
+            if(Math.pow(2, i) <= (tB & 0xff)) {
+                if(i == x) {
+                    System.out.println(true);
+                    return;
+                } else {
+                    tB = (byte)((int)(tB & 0xff) - Math.pow(2, i));
+                }
+            } else {
+                if(i == x) {
+                    System.out.println(false);
+                    return;
+                }
+            }
+        }
+
+
+        System.out.println(Math.pow(2, 7));
+    }
 }
