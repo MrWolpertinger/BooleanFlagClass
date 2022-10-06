047aa6a77bb76aa7b45613280d1b81e496b0588e
Derek Kropff <u19015501@tuks.co.za>
2022-10-06 08:00:01 +0200

    All Done

--- source/ByteBooleanArrayFlag.java
+++ source/ByteBooleanArrayFlag.java
@@ -2,10 +2,56 @@
 
 public class ByteBooleanArrayFlag {
     
-    private byte boolArray = (byte)129;
+    private byte boolArray;
 
-    public ByteBooleanArrayFlag() {}
-    public ByteBooleanArrayFlag(boolean[] bs) {}
+    public ByteBooleanArrayFlag() { boolArray = 0; }
+
+    public ByteBooleanArrayFlag(boolean[] bs) throws IllegalArgumentException{//bs.length is not looked at
+        if (bs.length < 8 || bs.length > 8) {
+            throw new IllegalArgumentException("ByteBooleanArrayFlag requires exactly 8 bytes");
+        }
+
+        boolArray = 0;
+        for (int i = 0; i < 8; i++) {
+            if(bs[7-i] == true) {
+                boolArray = (byte)((int)(boolArray & 0xff) + Math.pow(2, i));
+            }
+        }
+    }
+
+    public void printByte() {
+        System.out.println(boolArray);
+    }
+
+    public void printByteAsUnsigned() {
+        System.out.println((boolArray & 0xff));
+    }
+
+    public void printAsBooleanArray(boolean Flag) {
+        byte tB = boolArray;
+
+        System.out.print("{");
+        for(int i = 7; i >= 0; i--) {
+            if(Math.pow(2, i) <= (tB & 0xff)) {
+                tB = (byte)((int)(tB & 0xff) - Math.pow(2, i));
+                if(Flag == true) {
+                    System.out.print(" true ");
+                } else {
+                    System.out.print(" 1 ");
+                }
+            } else {
+                if(Flag == true) {
+                    System.out.print(" false ");
+                } else {
+                    System.out.print(" 0 ");
+                }
+            }
+            if(i > 0) {
+                System.out.print(",");
+            }
+        }
+        System.out.println("}");
+    }
 
     public boolean getFlag(int index) {//returns the flag value at the specified index
         //make sure inupt is correct
@@ -44,15 +90,23 @@ public class ByteBooleanArrayFlag {
         for(int i = 7; i >= 0; i--) {
             if(Math.pow(2, i) <= (tB & 0xff)) {
                 if(i == index) {
-                    System.out.println(true);//
-                    return true;
+                    if(value == true) {
+                        return true;
+                    } else {
+                        boolArray = (byte)((int)(boolArray & 0xff) - Math.pow(2, i));
+                        return true;
+                    }
                 } else {
                     tB = (byte)((int)(tB & 0xff) - Math.pow(2, i));
                 }
             } else {
                 if(i == index) {
-                    System.out.println(false);//
-                    return false;
+                    if(value == true) {
+                        boolArray = (byte)((int)(boolArray & 0xff) + Math.pow(2, i));
+                        return true;
+                    } else {
+                        return true;
+                    }
                 }
             }
         }
--- source/Main.java
+++ source/Main.java
@@ -1,29 +1,16 @@
 
 public class Main {
     public static void main(String[] args) {
-        byte b = (byte)129;
-        byte tB = b;
-        //retrieve index 6
-        int x = 6;
+        boolean argsForTest[] = {true, false, false, false, true, true, false, false};
 
-        //read indexed boolean value from a byte
-        for(int i = 7; i >= 0; i--) {
-            if(Math.pow(2, i) <= (tB & 0xff)) {
-                if(i == x) {
-                    System.out.println(true);
-                    return;
-                } else {
-                    tB = (byte)((int)(tB & 0xff) - Math.pow(2, i));
-                }
-            } else {
-                if(i == x) {
-                    System.out.println(false);
-                    return;
-                }
-            }
-        }
+        ByteBooleanArrayFlag b = new ByteBooleanArrayFlag(argsForTest);
 
 
-        System.out.println(Math.pow(2, 7));
+
+        
+        b.printByte();
+        b.printByteAsUnsigned();
+        b.printAsBooleanArray(false);
+        b.printAsBooleanArray(true);
     }
 }
